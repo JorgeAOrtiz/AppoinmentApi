14de8511318c9bca5760db0992814ae7fc985de5
JorgeAOrtiz <147191880+JorgeAOrtiz@users.noreply.github.com>
2025-02-28 10:48:17 -0500

    Add project files.

---
+++ AppoinmentCore/Interfaces/IAppoinmentService.cs
@@ -0,0 +1,13 @@
+﻿namespace AppoinmentCore.Interfaces
+{
+    public interface IAppoinmentService
+    {
+        void AddAppointment(Appointment appointment);
+        void UpdateAppointment(Appointment appointment);
+        void DeleteAppointment(int appointmentId);
+        IEnumerable<Appointment> ListAppointmentsByUser(int userId);
+        void ApproveAppointment(int appointmentId);
+        void CancelAppointment(int appointmentId);
+        IEnumerable<Appointment> ListAppointments();
+    }
+}
